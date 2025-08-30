24085494ac1535606fd509463a291de958b87177
XIAOZHUXUEJAVA <2393751426@qq.com>
2025-08-30 15:02:09 +0800

    any

--- src/lib/store/history-store.ts
+++ src/lib/store/history-store.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand'
 
 export interface HistoryAction {
   type: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   payload: any
   timestamp: number
 }
